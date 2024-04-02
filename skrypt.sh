4b634114a558309cde4483dfad605f62c3a11f1f
Vitoria2525 <torito2845828@gmail.com>
2024-04-02 21:15:40 +0300

    100 files

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,11 @@ if [[ $1 == "--date" ]]; then
    date
 fi
 
+if [[ $1 == "--logs" ]]; then
+  for ((i=1; i<=100; i++)); do
+    filename="log${i}.txt"
+    echo "Filename: $filename" > $filename
+    echo "Script: $0" >> $filename
+    echo "Creation date: $(date)" >> $filename
+  done
+fi
