cefda4ca2e2736c2cbe59060fed21ad4cfcf7f31
Vitoria2525 <torito2845828@gmail.com>
2024-04-02 21:22:08 +0300

    Obsługa argumentu liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -3,7 +3,14 @@ if [[ $1 == "--date" ]]; then
    date
 fi
 
-if [[ $1 == "--logs" ]]; then
+if [[ $1 == "--logs" && $2 -ge 1 && $2 -le 30 ]]; then
+  for ((i=1; i<=$2; i++)); do
+    filename="log${i}.txt"
+    echo "Filename: $filename" > $filename
+    echo "Script: $0" >> $filename
+    echo "Creation date: $(date)" >> $filename
+  done
+elif [[ $1 == "--logs" ]]; then
   for ((i=1; i<=100; i++)); do
     filename="log${i}.txt"
     echo "Filename: $filename" > $filename
